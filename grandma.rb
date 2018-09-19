620de432014f69116d8a79904670e5e99a687d25
Bridget Kalinda <bridgetkalinda@yahoo.co.uk>
2018-09-19 03:04:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,15 +13,15 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
     if phrase == "I LOVE YOU GRANDMA!"
-      puts "I LOVE YOU TOO PUMPKIN!"
+      return "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == phrase.downcase
-  		puts "HUH?! SPEAK UP, SONNY!"
+  		return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.downcase
-      puts "HUH?! SPEAK UP, SONNY!"
+      return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.upcase
-      puts "NO, NOT SINCE 1938!"
+      return "NO, NOT SINCE 1938!"
     else
-  		puts "NO, NOT SINCE 1938!"
+  		return "NO, NOT SINCE 1938!"
     end
 end
 
